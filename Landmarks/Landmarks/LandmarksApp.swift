4f6b57a5e3b527054e7ef60703feecc0873b5f77
Tyler O'Neil <tyleroneil523@gmail.com>
2024-04-21 11:18:22 -0800

    Initial Commit
    Starting Files for the project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tyler Oneil on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
