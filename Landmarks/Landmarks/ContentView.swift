34561404142bc57ac0e21e289050e40d2c1731f8
Tyler O'Neil <tyleroneil523@gmail.com>
2024-04-21 12:04:25 -0800

    Demo-Page
    Basic Page assembly has been complete! A note I wanted to make is the construction of the structures and view pages take sometime to find the right properties and autocomplete sucks so read the docs it's worth it.

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,25 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) { // Vertical stack to keep items in lnline top to bottom
+        VStack {
             
-            Text("Turtle Rock")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundStyle(.green)
+            MapView() // pulls in our mapview from our previous page
+                .frame(height: 300) // sets the area of the map
             
-            HStack { // Hstack is for our horizontal stacked or aligned text
+            CircleImage() // adding circle image view page in as well
+                .offset(y: -130)
+                .padding(.bottom, -130) // formatting moves the image for effect and readability
+            
+            VStack(alignment: .leading) { // Vertical stack to keep items in lnline top to bottom
                 
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                    .fontWeight(.medium)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-                    .fontWeight(.medium)
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.green)
+                
+                HStack { // Hstack is for our horizontal stacked or aligned text
+                    
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                    
+                }
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
+                
+                Divider() // Inserts soft line divider for cleanliness
+
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Descriptive text goes here.")
             }
+            .padding() // keeps proper spacing from edges
+            Spacer() // Pushing our content up to the top of the screen
         }
-        .padding() // padding is keeping our text items from touching the very edge of the screen
     }
 }
 
