6ef52213b1acf92e3015dab6c7dfb1a8888e8a9c
Tyler O'Neil <tyleroneil523@gmail.com>
2024-04-21 12:38:52 -0800

    Data-Decoding
    Inserted the json file for the app to be able to ingest and assign based on the Landmark structure I defined in the previous step

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,44 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Tyler Oneil on 4/21/24.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+
+struct Landmark: Hashable, Codable { // Codable is what will Decoable useful later on for the decoding of the JSON file
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    
+    private var imageName: String // Reads the image name from the data
+        var image: Image { // Loads the image name from the property of the pngs
+            Image(imageName) // asserts the string name to Image name
+        }
+    
+    
+    
+    private var coordinates: Coordinates // Using our Nested Coordiantes property we will be able to display the coordinate data in the file
+    
+    var locationCoordinate: CLLocationCoordinate2D { // calculates our longitude and latitude through required use of locationCoordinate property. Allows use of MapKit
+            CLLocationCoordinate2D(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude)
+        }
+    
+    
+
+        struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
+    
+    
+}
---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,37 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Tyler Oneil on 4/21/24.
+//
+
+import Foundation
+
+
+var landmarks: [Landmark] = load("landmarkData.json") // Creates the array of landmarks when initialized from the JSON file
+
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data // If I am underststanding the docs correctly the Data structure itself reserves Byte buffers for memory in this simple init() method
+
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
